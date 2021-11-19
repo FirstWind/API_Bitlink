13d9e89c55be62947f95dd79204640135a0f8a67
FirstWind <FirstWind@mail.ru>
2021-11-19 22:20:38 +0900

    rewrited kod

--- main.py
+++ main.py
@@ -4,56 +4,53 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 
-def shorten_link(url_user):
-    url = "https://api-ssl.bitly.com/v4/bitlinks"
-    body = {"long_url": url_user, "title": "Битлинк"}
-    response = requests.post(url, headers=header, json=body)
-    if response.ok:
-        return response
-    else:
-        response.raise_for_status()
+def shorten_link(url_bitly, url):
+    body = {"long_url": url, "title": "Битлинк: "}
+    r = requests.post(url_bitly, headers=header, json=body)
+    r.raise_for_status()
+    return r
 
 
-def is_bitlink(url_user):
-    url_parse = urlparse(url_user)
-    bitlink = url_parse.netloc + url_parse.path
-    url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
-    response = requests.get(url, headers=header)
-    if response.ok:
-        return bitlink
-    else:
-        response.raise_for_status()
+def is_bitlink(url_bitly, bitlink):
+    url = f"{url_bitly}{bitlink}"
+    r = requests.get(url, headers=header)
+    return r.ok
 
 
-def count_clicks(bitlink):
-    url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
-    response = requests.get(url, headers=header)
-    if response.ok:
-        return response
-    else:
-        response.raise_for_status()
+def count_clicks(url_bitly, bitlink):
+    url = f"{url_bitly}{bitlink}/clicks/summary"
+    r = requests.get(url, headers=header)
+    r.raise_for_status()
+    return r
+
+
+def parts_json(json, *args):
+    result = ""
+    for element in args:
+        result += str(json[element])
+    return result
 
 
 if __name__ == "__main__":
+    url_bitly = "https://api-ssl.bitly.com/v4/bitlinks/"
+
     load_dotenv(find_dotenv())
     header = {"Authorization": f"Bearer {os.environ.get('TOKEN_BITLY')}"}
 
-    url_user = input("Input url https://")
-    if not urlparse(url_user).scheme:
-        url_user = f"https://{url_user}"
-    else:
-        url_user = f"{url_user}"
+    input_url = input("Input url: ")
+    url_parsed = urlparse(input_url, scheme='https')
+    url_bitlink = url_parsed.netloc + url_parsed.path
+    if not url_parsed.scheme:
+        input_url = f"{url_parsed.scheme}://{input_url}"
 
     try:
-        url_response_is_bitlink = is_bitlink(url_user)
-        response_count_clicks = count_clicks(url_response_is_bitlink)
-        print(f"По вашей ссылке прошли {response_count_clicks.json()['total_clicks']} раз(a)")
-    except requests.HTTPError as e:
-        if e.response.status_code == 404:
-            try:
-                response_shorten_link = shorten_link(url_user)
-                print(response_shorten_link.json()["title"], response_shorten_link.json()["link"])
-            except requests.HTTPError as e:
-                print("Ошибка: {}".format(e))
+        if is_bitlink(url_bitly, url_bitlink):
+            response_count_clicks = count_clicks(url_bitly, url_bitlink)
+            clicks = parts_json(response_count_clicks.json(), "total_clicks")
+            print(f"По вашей ссылке прошли {clicks} раз(a)")
         else:
-            print("Ошибка: {}".format(e))
+            response_shorten_link = shorten_link(url_bitly, input_url)
+            clicks = parts_json(response_shorten_link.json(), "title", "link")
+            print(clicks)
+    except requests.HTTPError as e:
+        print("Ошибка: {}".format(e))
